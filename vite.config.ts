7adc48fe5e1b085fe70d4e68f2187b0950930bbe
Jeancoman <jeanfranbg@gmail.com>
2023-10-05 05:31:28 -0430

    Hola

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     // Some libraries use the global object, even though it doesn't exist in the browser.
     // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
     // https://github.com/vitejs/vite/discussions/5912
-    global: {},
+    define: {
+      _global: ({})
+  }
   },
 })
